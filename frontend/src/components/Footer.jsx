a2dcbbccb45147ab85c0a1547bd89a84192766c4
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-06-25 17:39:25 +0530

    Added CSS in Footer and Successfully Create Home Page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,12 +3,12 @@ import { assets } from "./../assets/assets";
 
 const Footer = () => {
   return (
-    <div>
-      <div>
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         {/* -----------------LEFT SECTION------------ */}
         <div>
-          <img src={assets.logo} alt="" />
-          <p>
+          <img className="mb-5 w-40" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
@@ -18,8 +18,8 @@ const Footer = () => {
 
         {/* -----------------CENTER SECTION------------ */}
         <div>
-          <p>COMPANY</p>
-          <ul>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>Home</li>
             <li>About us</li>
             <li>Delivery</li>
@@ -29,8 +29,8 @@ const Footer = () => {
 
         {/* -----------------RIGHT SECTION------------ */}
         <div>
-          <p>GET IN TOUCH</p>
-          <ul>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>+91 79707 85645</li>
             <li>harshikeshkoili214@gmail.com</li>
           </ul>
@@ -40,7 +40,9 @@ const Footer = () => {
       {/* -----------Copyright Text----------- */}
       <div>
         <hr />
-        <p>Copyright 2025 @ Harshikesh.dev - All Right Reserved.</p>
+        <p className="py-5 text-sm text-center">
+          Copyright 2025 @ Harshikesh.dev - All Right Reserved.
+        </p>
       </div>
     </div>
   );
