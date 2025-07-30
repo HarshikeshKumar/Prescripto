b078b1b3845f2d36815945b7ca5269b328143f61
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-07-30 14:50:26 +0530

    Rerendering of my-appointment page on UI

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "./../context/AppContext";
 
 const MyAppointments = () => {
-  return <div></div>;
+  const { doctors } = useContext(AppContext);
+
+  return (
+    <div>
+      <p>My Appointments</p>
+      <div>
+        {doctors.slice(0, 2).map((item, index) => (
+          <div key={index}>
+            <div>
+              <img src={item.image} alt="" />
+            </div>
+            <div>
+              <p>{item.name}</p>
+              <p>{item.speciality}</p>
+              <p>Address:</p>
+              <p>{item.address.line1}</p>
+              <p>{item.address.line2}</p>
+              <p>
+                <span>Date & Time:</span>25, july, 2024 | 8:30 PM
+              </p>
+            </div>
+            <div></div>
+            <div>
+              <button>Pay Online</button>
+              <button>Cancel appointment</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyAppointments;
